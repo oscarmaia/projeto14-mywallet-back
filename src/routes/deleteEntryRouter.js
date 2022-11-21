829b1d4cd3359183b80932e6a815f1e02f62b4d6
Oscar Maia <oscarfgmaia@gmail.com>
2022-11-21 01:16:36 -0300

    delete entry router created

--- src/routes/deleteEntryRouter.js
+++ src/routes/deleteEntryRouter.js
@@ -2,5 +2,5 @@ import { Router } from 'express';
 import { deleteEntry } from '../controllers/user.controller.js';
 
 const router = Router();
-router.get('/main/entry:id', deleteEntry);
+router.delete('/main/entry:id', deleteEntry);
 export default router;
